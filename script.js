dc6a7a222f09c5c84015e978741eaa51308dedba
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-08-23 14:12:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+
+document.getElementById("btn").addEventListener("click",(event)=>{
+	event.preventDefault()
+	let name = document.getElementById("name").value;
+	let age = document.getElementById("age").value;
+
+	new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+				resolve()
+			}else{
+				reject()
+			}
+		},4000)
+	}).then(()=>{
+		alert(`Welcome, ${name}. You can vote.`)
+	}).catch(()=>{
+		alert(`Oh sorry ${name}. You aren't old enough.`)
+	})
+	
+	
+})
